643ccbf23610b2c5186b062b3734a0d71097f9c9
aliakseimilaradau <leksa8858@gmail.com>
2026-06-30 16:56:42 +0300

    Add age category with if-else logic.

--- AboutMe/Program.cs
+++ AboutMe/Program.cs
@@ -4,10 +4,26 @@ string name = Console.ReadLine();
 Console.WriteLine("Enter your age: ");
 int age = int.Parse(Console.ReadLine());
 
+if (age < 13)
+{
+    Console.WriteLine("You are a child.");
+}
+else if (age >= 13 && age < 18)
+{
+    Console.WriteLine("You are a teenager.");
+}
+else if (age >= 18 && age < 65)
+{
+    Console.WriteLine("You are an adult.");
+}
+else
+{
+    Console.WriteLine("You are a senior.");
+}
+
 Console.WriteLine("Enter your height in meters: ");
 double height = double.Parse(Console.ReadLine());
 
 Console.WriteLine("Your name is " + name + ", you are " + age + " years old and your height is " + height + " m.");
-const int currentYear = 2026;
-Console.WriteLine($"You were born around the year { currentYear - age}.");
-
+int Year = 2026 - age;
+Console.WriteLine($"You were born around the year {Year}.");
