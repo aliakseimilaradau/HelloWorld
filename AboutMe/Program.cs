722b97c3692f409e95dc4489fe282af291e841a3
aliakseimilaradau <leksa8858@gmail.com>
2026-06-30 16:35:28 +0300

    Add AboutMe console app with user info.

---
+++ AboutMe/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter your age: ");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your height in meters: ");
+double height = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Your name is " + name + ", you are " + age + " years old and your height is " + height + " m.");
+const int currentYear = 2026;
+Console.WriteLine($"You were born around the year { currentYear - age}.");
+
